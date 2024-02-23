89bc5ae3b533f5ce026b76ef7161ee728bb5e110
sierra-evans <eva22011@byui.edu>
2024-02-23 10:16:28 -0700

    Minor changes

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Data;
-using System.Security.Cryptography.X509Certificates;
-
 class Program
 {
     static void Main(string[] args)
--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel.DataAnnotations;
-
 public class Scripture {
     private Reference reference;
     private List<Word> words;
