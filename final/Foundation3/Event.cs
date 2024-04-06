d7b05ecbf69a5b88055f497200f16624a985359d
sierra-evans <eva22011@byui.edu>
2024-04-06 09:54:07 -0600

    Fixed error

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -27,22 +27,17 @@ public class Event
 
     public void DisplayStandard()
     {
-        // Lists the title, description, date, time, and address.
-        
+
         Console.WriteLine($"Title : {title}\nDescription: {description}\nDate: {date}\nTime: {time}\nAddress:\n{address.GetDisplayAddress()}");
     }
 
     public void DisplayShort()
     {
-        // Lists the type of event, title, and the date.
-
         Console.WriteLine($"Event Type: {type}\nTitle: {title}\nDate: {date}");
     }
 
     public virtual void DisplayFull()
     {
-        // Lists all of the above, plus type of event and information specific to that event type. For lectures, this includes the speaker name and capacity. For receptions this includes an email for RSVP. For outdoor gatherings, this includes a statement of the weather.
-
         Console.WriteLine($"Event Type: {type}\nTitle : {title}\nDescription: {description}\nDate: {date}\nTime: {time}\nAddress:\n{address.GetDisplayAddress()}");
     }
 }
